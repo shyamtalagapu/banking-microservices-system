eccdb201d4ca0704761ec5206b087bb74f206156
shyamtalagapu <shyamtalagapu710@gmail.com>
2026-05-12 07:02:05 +0530

    port fixation

--- customer-service/src/main/java/com/banking/customer/authRegister/AuthFeignClient.java
+++ customer-service/src/main/java/com/banking/customer/authRegister/AuthFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.banking.customer.auth.dto.AuthRegisterRequest;
 
-@FeignClient(name = "auth-service", url = "http://auth-service")
+@FeignClient(name = "auth-service", url = "http://auth-service:8083")
 public interface AuthFeignClient {
 	
 	@PostMapping("/auth/register")
